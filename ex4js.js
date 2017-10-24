832e9aaa6c7b8d629b692e263b1634a9e3d65587
loicmagny <loicmagn60@gmail.com>
2017-10-24 09:41:37 +0200

    ex4js

---
+++ ex4js.js
@@ -0,0 +1,6 @@
+var first, second, result;
+lastname  = prompt('Entrez votre nom :');
+firstname = prompt('Entrez votre prénom :');
+town = prompt('Ou vivez vous?');
+result = 'Bonjour monsieur ' + lastname + ' ' + firstname +','+' Vous vivez a ' + town;
+alert(result);
